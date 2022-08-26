4a00d06ad736e5e4e0ffada5b7dfdabb44616849
ali-opd <nurali.batamindo@gmail.com>
2022-08-26 21:50:53 +0700

    Initial commit

---
+++ serverless.ts
@@ -0,0 +1,64 @@
+import type { AWS } from '@serverless/typescript';
+import functions from './serverless/functions';
+import dynamoResources from './serverless/dynamoResources';
+
+const serverlessConfiguration: AWS = {
+  service: 'urlshortener',
+  frameworkVersion: '3',
+  plugins: ['serverless-esbuild'],
+  provider: {
+    name: 'aws',
+    runtime: 'nodejs14.x',
+    profile: 'sam-williams-course',
+    region: 'ap-southeast-1',
+    iamRoleStatements: [
+      {
+        Effect: 'Allow',
+        Action: 'dynamodb:*',
+        Resource:
+          'arn:aws:dynamodb:${self:provider.region}:${aws:accountId}:table/${self:custom.urlTableName}',
+      },
+    ],
+    apiGateway: {
+      minimumCompressionSize: 1024,
+      shouldStartNameWithService: true,
+    },
+    environment: {
+      AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
+      NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      urlTable: '${self:custom.urlTableName}',
+      baseUrl: {
+        'Fn::Join': [
+          '',
+          [
+            'https://',
+            { Ref: 'httpApi' },
+            '.execute-api.${self:provider.region}.amazonaws.com',
+          ],
+        ],
+      },
+    },
+  },
+  functions,
+  resources: {
+    Resources: {
+      ...dynamoResources,
+    },
+  },
+  package: { individually: true },
+  custom: {
+    urlTableName: '${sls:stage}-url-table',
+    esbuild: {
+      bundle: true,
+      minify: false,
+      sourcemap: true,
+      exclude: ['aws-sdk'],
+      target: 'node14',
+      define: { 'require.resolve': undefined },
+      platform: 'node',
+      concurrency: 10,
+    },
+  },
+};
+
+module.exports = serverlessConfiguration;
---
+++ src/functions/setUrl/index.ts
@@ -0,0 +1,34 @@
+import { v4 as uuid } from 'uuid';
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import { formatJSONResponse } from '@libs/apiGateway';
+
+import { dynamo } from '@libs/dynamo';
+
+export const handler = async (event: APIGatewayProxyEvent) => {
+  try {
+    const body = JSON.parse(event.body);
+    const tableName = process.env.urlTable;
+    const baseUrl = process.env.baseUrl;
+    const originalUrl = body.url;
+
+    const code = uuid().slice(0, 8);
+    const shortUrl = `${baseUrl}/${code}`;
+    const data = {
+      id: code,
+      shortUrl,
+      originalUrl,
+    };
+
+    await dynamo.write(data, tableName);
+
+    return formatJSONResponse({
+      data: { shortUrl, originalUrl },
+    });
+  } catch (error) {
+    console.log(error);
+    return formatJSONResponse({
+      statusCode: 500,
+      data: { message: error.message },
+    });
+  }
+};
